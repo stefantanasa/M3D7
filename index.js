2977bed7bc1f9974e568cc66ed23e2bc1a8efde8
stefantanasa <liketanasa@gmail.com>
2021-12-07 14:43:27 +0000

    update

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-let query = "@";
-
 window.onload = function () {
   fetch("https://jsonplaceholder.typicode.com/users")
     .then((resp) => resp.json())
     .then((usersArray) => {
       console.log(usersArray);
+      showNames(usersArray, "name");
+      usersArray.forEach((user) => console.log(user.name));
     });
+};
+
+let showAddress = function (array) {
+  array.forEach((user) => {
+    address = [
+      user.address["city"],
+      user.address["street"],
+      user.address["suite"],
+      user.address["zipcode"],
+    ];
+    console.log(address.join(", "));
+  });
+};
+
+const showNames = function (array, item) {
+  array.forEach((element) => console.log(element[item]));
 };
